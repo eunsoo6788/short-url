05b8ee99bed696ab7ca6a44288fbda8b0607e73c
eunsoojin <ensu6788@gmail.com>
2026-05-31 00:31:27 +0900

    first commit

---
+++ src/main/kotlin/toy/two/shorturl/ShortUrlApplication.kt
@@ -0,0 +1,11 @@
+package toy.two.shorturl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortUrlApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShortUrlApplication>(*args)
+}
---
+++ src/test/kotlin/toy/two/shorturl/ShortUrlApplicationTests.kt
@@ -0,0 +1,13 @@
+package toy.two.shorturl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
